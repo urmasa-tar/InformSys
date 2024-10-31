24dc085bd08efe729bb3da6de1f9449b9eb851c2
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-10-31 19:05:59 +0300

    ui library

--- mainAplication/Form1.Designer.cs
+++ mainAplication/Form1.Designer.cs
@@ -57,11 +57,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(126, 57);
+            this.label1.Location = new System.Drawing.Point(192, 74);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 34);
+            this.label1.Size = new System.Drawing.Size(139, 34);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Analytics";
             // 
             // Form1
             // 
