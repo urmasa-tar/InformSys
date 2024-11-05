8d8b2f6cba5a7e7fd244cb09b42db177a0d39338
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-11-05 21:31:38 +0300

    addColors

--- FlatUI/ThemeColor.cs
+++ FlatUI/ThemeColor.cs
@@ -8,7 +8,35 @@ namespace FlatUI
 {
     public class ThemeColor
     {
-        List<String> colorName = new List<string>() { };
+        public List<String> colorName = new List<string>() {
+        "#3F5185",
+        "#009688",
+        "#FF5722",
+        "#607d88",
+        "#FF9800",
+        "#9C27B0",
+        "#2196f3",
+        "#EA676C",
+        "#E41A4A",
+        "#5978BB",
+        "#018790",
+        "#0E3441",
+        "#00B0AD",
+        "#721d47",
+        "#EA4833",
+        "#EF937E",
+        "#F37521",
+        "#A12059",
+        "#126881",
+        "#8bc240",
+        "#364d5b",
+        "#C7DC5B",
+        "#0094BC",
+        "#E4126B",
+        "#43B76E",
+        "#7BCFE9",
+        "#871C46"
+        };
 
     }
 }
